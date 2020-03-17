44c3a8061b2dffc7323096f1cd8f72b0de4df8c5
Enda Lee <enda@endalee.com>
2020-03-17 18:48:37 +0000

    add websocket

---
+++ javascript/mqtt/websockets.js
@@ -0,0 +1,98 @@
+// var for mqqt object instance
+let mqtt;
+const reconnectTimeout = 2000;
+// Websocket host
+const host = 'broker.hivemq.com';
+// Websocket port
+const port = 8000;
+// Message delimiter char
+const msgDelimiter = ',';
+
+const subTopic = 'messageapp/#';
+
+const testTopic = 'messageapp/sender/createdAt/subject/message';
+
+// moment used to generate date 
+const testMessage = `me@me.com${msgDelimiter}${moment().format('llll').replace(/,/g, ' ')}${msgDelimiter}subject${msgDelimiter}message`;
+
+// Page element ref for messages
+let messageList = document.getElementById("messageList");
+
+// Attempt connect to mqtt via websocket
+function MQTTconnect() {
+  // Log nessage to console
+  console.log(`connecting to ${host}:${port}`);
+
+  // Get mqtt instance
+  mqtt = new Paho.MQTT.Client(host, port, "clientjs");
+  const options = {
+    timeout: 3,
+    onSuccess: onConnect,
+    onFailure: onFailure
+  };
+  mqtt.onMessageArrived = onMessageArrived;
+
+  mqtt.connect(options); //connect
+}
+
+// on sucessful connect to mqtt over websocket
+function onConnect() {
+  // Once a connection has been made, make a subscription and send a message.
+
+  console.log('Connected');
+  mqtt.subscribe(subTopic);
+
+  // send a test message
+  message = new Paho.MQTT.Message(testMessage);
+  message.destinationName = defaultTopic;
+  mqtt.send(message);
+}
+
+// Handle errors
+function onFailure(message) {
+  // Log error in console
+  console.log(`Connection Attempt to Host ${host} Failed`);
+  setTimeout(MQTTconnect, reconnectTimeout);
+}
+
+// When a message is recieved on websocket connection
+function onMessageArrived(message) {
+  const topic = message.destinationName;
+  const payload = message.payloadString;
+
+  // Split payload string into an array
+  const msgArr = payload.split(msgDelimiter);
+
+  // Add to the page by adding to start ofthe page element 
+  messageList.innerHTML = `<li class="list-group-item">
+                              <b>Date:</b> ${msgArr[1]}
+                              <b>From:</b> ${msgArr[0]}
+                              <b>Subject:</b> ${msgArr[2]}
+                              <b>Message:</b> ${msgArr[3]}
+                          </li>
+  ${messageList.innerHTML}`;
+}
+
+// Send a message when form is submitted
+function sendMessage() {
+
+  // Read values from form
+  const topic = document.getElementById("topic").value;
+  const sender = document.getElementById("sender").value;
+  const subject = document.getElementById("subject").value;
+  const messageText = document.getElementById("messageText").value; 
+
+  // Construct message
+  const payload = `'${sender}'${msgDelimiter}'${moment().format('llll').replace(/,/g, ' ')}'${msgDelimiter}'${subject}'${msgDelimiter}'${messageText}'`;
+  console.log(`playload: ${payload}`)
+  message = new Paho.MQTT.Message(payload);
+  message.destinationName = topic;
+  // Send message via websocket
+  mqtt.send(message);   
+
+  // Prevent browser from reseting page after submit has been handled
+  return false;
+}
+
+// Connect mqtt
+MQTTconnect();
--- messages.sh
+++ messages.sh
@@ -3,6 +3,12 @@
 # we can use a local mosquitto broker OR a web based one (local commented out below)
 # the while will stay in the subscribe loop and act on any messages rxed
 #
+#
+# test this script
+# ./messages.sh messageapp/sender/createdAt/subject/message -m me,today,subject,message
+# pub
+# mosquitto_pub -h broker.hivemq.com -t messageapp/sender/createdAt/subject/message -m me,today,subject,message
+#
 #vars
 #
 # Firebase Vars - from app settings
@@ -31,41 +37,47 @@ REQ_METHOD='POST'
 mosquitto_sub -v -h broker.hivemq.com -t messageapp/# | while read line
 do
         # first all we do is echo the line (topic + message) to the screen
-        echo $line
+        echo "line: $line\n"
 
         # assume topic has 4 fields in form field1/field2/field3/field4
         # cut them out of the topic and put into column vars 1-4
         column1=`echo $line|cut -f2 -d/`
-        echo $column1
+        echo "col1: $column1"
         column2=`echo $line|cut -f3 -d/`
-        echo $column2
+        echo "col2: $column2"
         column3=`echo $line|cut -f4 -d/`
-        echo $column3
+        echo "col3: $column3"
         column4=`echo $line|cut -f5 -d/`
-        echo $column4
-        column5=`echo $line|cut -f6 -d/`
-        echo $column5             
+        echo -e "col4: $column4\n"            
 
         # now we work on the message
-        # assume message has 3 fields in form field1,field2,field3
-        # cut them out of the msg and put into column vars 4-6
-        msg=`echo $line|cut -f2 -d' '`
-        column6=`echo $msg|cut -f1 -d,`
-        echo $column6
-        column7=`echo $msg|cut -f2 -d,`
-        echo $column7
-        column8=`echo $msg|cut -f3 -d,`
-        echo $column8
-        column9=`echo $msg|cut -f4 -d,`
-        echo $column9
+        # assume message has 5 fields in form field1,field2,field3,field4
+        # cut them out of the msg and put into column vars
+        msg=`echo "$line"|cut -f2- -d ' '`
+        echo -e "msg: $msg\n"
+        sender=`echo "$msg"|cut -f1 -d,`
+        echo "sender: $sender"
+        createdAt=`echo "$msg"|cut -f2 -d,`
+        echo "createdAt: $createdAt"
+        subject=`echo "$msg"|cut -f3 -d,`
+        echo "Subject: $subject"
+        message=`echo "$msg"|cut -f4 -d,`
+        echo "message: $message"
         #
         # now we work on the DB, we want to out the columns(1-6) into the DB
         #
-        # generate date stamp
-        now=`date`
+        # If $createdAt has a value, use it, otherwise generate a timestamp
+        if [[ $createdAt ]]
+        then
+            now=$createdAt
+        else
+            # generate date stamp
+            now=`date`
+        fi
+
         # Data to be sent in JSON format
-        JSON='{"fields": {"createdAt": {"stringValue": "'"$now"'"}, "sender": {"stringValue": "'"$column6"'"}, "subject": {"stringValue": "'"$column8"'"}, "message": {"stringValue": "'"$column9"'"}}}'
-        
+        JSON='{"fields": {"createdAt": {"stringValue": "'"$now"'"}, "sender": {"stringValue": "'"$sender"'"}, "subject": {"stringValue": "'"$subject"'"}, "message": {"stringValue": "'"$message"'"}}}'
+        echo $JSON
         # https://askubuntu.com/questions/1162945/how-to-send-json-as-variable-with-bash-curl
 
         # Use CURL to send POST request + data
@@ -74,11 +86,3 @@ do
         # Show response
         echo $response
 done
-
-# test
-
-# this script
-# ./messages.sh messageapp/sender/createdAt/subject/message -m me,today,subject,message
-
-# pub
-# mosquitto_pub -h broker.hivemq.com -t messageapp/sender/createdAt/subject/message -m me,today,subject,message
